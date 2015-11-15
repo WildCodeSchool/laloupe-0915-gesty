eecb30d5f0956b2da5d316bdd08497110b304a2f
Sev28 <charlisev@hotmail.fr>
2015-11-15 20:16:44 +0100

    changement du profile user

--- src/Gesty/GestyBundle/Controller/DefaultController.php
+++ src/Gesty/GestyBundle/Controller/DefaultController.php
@@ -8,6 +8,13 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('GestyGestyBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('GestyGestyBundle:Default:index.html.twig');
     }
+
+    public function registerAction()
+    {
+        return $this->render('GestyGestyBundle:Default:register.html.twig');
+
+    }
+
 }
