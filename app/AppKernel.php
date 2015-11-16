ae0a3923be595db95c4c4405037da9e0e498e5b8
Mika28110 <michael.godinhodacosta@hotmail.fr>
2015-11-16 11:05:37 +0100

    ajout entite eleve

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -48,4 +48,9 @@ class AppKernel extends Kernel
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
+
+    public function init() {
+        date_default_timezone_set( 'Europe/Paris' );
+        parent::init();
+    }
 }
---
+++ src/Gesty/GestyBundle/Entity/Entity/Eleve.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace Gesty\GestyBundle\Entity\Entity;
+
+/**
+ * Eleve
+ */
+class Eleve
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $nom;
+
+    /**
+     * @var string
+     */
+    private $prenom;
+
+    /**
+     * @var \DateTime
+     */
+    private $dateDeNaissance;
+
+    /**
+     * @var string
+     */
+    private $ecole;
+
+    /**
+     * @var string
+     */
+    private $classe;
+
+    /**
+     * @var string
+     */
+    private $instituteur;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Eleve
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     *
+     * @return Eleve
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set dateDeNaissance
+     *
+     * @param \DateTime $dateDeNaissance
+     *
+     * @return Eleve
+     */
+    public function setDateDeNaissance($dateDeNaissance)
+    {
+        $this->dateDeNaissance = $dateDeNaissance;
+
+        return $this;
+    }
+
+    /**
+     * Get dateDeNaissance
+     *
+     * @return \DateTime
+     */
+    public function getDateDeNaissance()
+    {
+        return $this->dateDeNaissance;
+    }
+
+    /**
+     * Set ecole
+     *
+     * @param string $ecole
+     *
+     * @return Eleve
+     */
+    public function setEcole($ecole)
+    {
+        $this->ecole = $ecole;
+
+        return $this;
+    }
+
+    /**
+     * Get ecole
+     *
+     * @return string
+     */
+    public function getEcole()
+    {
+        return $this->ecole;
+    }
+
+    /**
+     * Set classe
+     *
+     * @param string $classe
+     *
+     * @return Eleve
+     */
+    public function setClasse($classe)
+    {
+        $this->classe = $classe;
+
+        return $this;
+    }
+
+    /**
+     * Get classe
+     *
+     * @return string
+     */
+    public function getClasse()
+    {
+        return $this->classe;
+    }
+
+    /**
+     * Set instituteur
+     *
+     * @param string $instituteur
+     *
+     * @return Eleve
+     */
+    public function setInstituteur($instituteur)
+    {
+        $this->instituteur = $instituteur;
+
+        return $this;
+    }
+
+    /**
+     * Get instituteur
+     *
+     * @return string
+     */
+    public function getInstituteur()
+    {
+        return $this->instituteur;
+    }
+}
+
