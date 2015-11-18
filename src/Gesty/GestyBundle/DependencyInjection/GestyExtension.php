0c2283223df4128ee94243b10eb1e3412c05d3f6
Sev28 <charlisev@hotmail.fr>
2015-11-18 14:43:29 +0100

    essai template sonata registartion

--- src/Gesty/GestyBundle/DependencyInjection/GestyExtension.php
+++ src/Gesty/GestyBundle/DependencyInjection/GestyExtension.php
@@ -23,6 +23,6 @@ class GestyExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('admin.yml');
+        $loader->load('services.yml');
     }
 }
