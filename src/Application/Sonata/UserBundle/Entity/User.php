690fad65b9c3b95e66a9fba37b88becbb2e54d33
Ishimura413 <superouiouil@gmail.com>
2015-12-07 20:40:59 +0100

    nom et prenom ajoutés a entity user et formulaire coordonnees peaufiné

--- src/Application/Sonata/UserBundle/Entity/User.php
+++ src/Application/Sonata/UserBundle/Entity/User.php
@@ -91,6 +91,15 @@ class User extends BaseUser
     /** Generate code */
 
 
+    /**
+     * @var string
+     */
+    private $nom;
+
+    /**
+     * @var string
+     */
+    private $prenom;
 
     /**
      * @var string
@@ -142,6 +151,54 @@ class User extends BaseUser
      */
     private $eleves;
 
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return User
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     *
+     * @return User
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
 
     /**
      * Set adresse
