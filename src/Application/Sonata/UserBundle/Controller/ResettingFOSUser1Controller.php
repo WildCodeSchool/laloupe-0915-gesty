855714deb5a6700888c61984a5c8eb20c1320b02
Sev28 <charlisev@hotmail.fr>
2016-01-25 09:49:06 +0100

    configuration du resetting password

--- src/Application/Sonata/UserBundle/Controller/ResettingFOSUser1Controller.php
+++ src/Application/Sonata/UserBundle/Controller/ResettingFOSUser1Controller.php
@@ -11,7 +11,9 @@
 
 namespace Application\Sonata\UserBundle\Controller;
 
-use Application\Sonata\UserBundle\Controller\ResettingController;
+use FOS\UserBundle\Controller\ResettingController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 
 /**
@@ -21,7 +23,38 @@ use Application\Sonata\UserBundle\Controller\ResettingController;
  *
  * @author Hugo Briand <briand@ekino.com>
  */
-class ResettingFOSUser1Controller extends \Application\Sonata\UserBundle\Controller\ResettingController
+class ResettingFOSUser1Controller extends \Sonata\UserBundle\Controller\ResettingFOSUser1Controller
 {
+    /**
+     * Reset user password
+     */
+    public function resetAction($token)
+    {
+        $user = $this->container->get('fos_user.user_manager')->findUserByConfirmationToken($token);
 
+        if (null === $user) {
+            throw new NotFoundHttpException(sprintf('The user with "confirmation token" does not exist for value "%s"', $token));
+        }
+
+        if (!$user->isPasswordRequestNonExpired($this->container->getParameter('fos_user.resetting.token_ttl'))) {
+            return new RedirectResponse($this->container->get('router')->generate('fos_user_resetting_request'));
+        }
+
+        $form = $this->container->get('fos_user.resetting.form');
+        $formHandler = $this->container->get('fos_user.resetting.form.handler');
+        $process = $formHandler->process($user);
+
+        if ($process) {
+            $this->setFlash('fos_user_success', 'Votre mot de passe à bien été modifié');
+            $response = new RedirectResponse($this->container->get('router')->generate('sonata_user_security_login'));
+
+
+            return $response;
+        }
+
+        return $this->container->get('templating')->renderResponse('ApplicationSonataUserBundle:Resetting:reset.html.'.$this->getEngine(), array(
+            'token' => $token,
+            'form' => $form->createView(),
+        ));
+    }
 }
