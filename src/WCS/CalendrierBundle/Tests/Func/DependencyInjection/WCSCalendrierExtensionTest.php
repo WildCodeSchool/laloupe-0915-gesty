714321aef8ef7f86e48e5366915302e940021d1e
Rodney Figaro <rodneyfigaro@hotmail.com>
2016-06-14 13:40:24 +0200

    Correction environnement test

--- src/WCS/CalendrierBundle/Tests/Func/DependencyInjection/WCSCalendrierExtensionTest.php
+++ src/WCS/CalendrierBundle/Tests/Func/DependencyInjection/WCSCalendrierExtensionTest.php
@@ -2,16 +2,20 @@
 /*
  * Ce test permet de s'assurer que le service se charge correctement
  */
-namespace WCS\CalendrierBundle\Tests\Integration\DependencyInjection;
+namespace src\WCS\CalendrierBundle\Tests\Func\DependencyInjection;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class WCSCalendrierExtensionTest extends KernelTestCase
 {
     public function testGetService()
     {
-        self::bootKernel();
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $app = new Application($kernel);
+        $app->setAutoExit(false);
 
-        $container = static::$kernel->getContainer();
+        $container = $kernel->getContainer();
 
         $calendrier = $container->get('wcs.calendrierscolaire');
         $this->assertInstanceOf('WCS\CalendrierBundle\Service\Service', $calendrier);
