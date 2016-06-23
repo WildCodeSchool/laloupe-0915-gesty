5cd582d2c548acbe3a38c8ed70541d10a4c1ea6b
martinezmanuel <martinez-manuel@neuf.fr>
2016-06-23 16:59:50 +0200

    page stat mensuelle

--- src/WCS/CantineBundle/Controller/StatController.php
+++ src/WCS/CantineBundle/Controller/StatController.php
@@ -12,18 +12,18 @@ use WCS\CalendrierBundle\Service\Periode\Periode;
 class StatController extends Controller
 {
 
-    public function getContainer()
+    public function countAction()
     {
-        return $this->redirect($this->generateUrl('WCS/CantineBundle/Ressources/views/statmonthly.html.twig'));
+        return $this->render('WCSCantineBundle:Block:statmonthly.html.twig');
     }
 
-    public function count()
+    /*public function countAction()
     {
         return $this->createQueryBuilder('s')
             ->select('COUNT(s)')
             ->getQuery()
             ->getSingleScalarResult();
     }
-    
+*/
     
 }
