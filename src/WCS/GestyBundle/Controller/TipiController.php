4dc4f39d6640ff0a527ffa9c5fc5bda015f13403
Celine Langlade <Cendrillon@MacBook-Pro-de-Celine.local>
2016-09-09 10:53:38 +0200

    numéro de téléphone

--- src/WCS/GestyBundle/Controller/TipiController.php
+++ src/WCS/GestyBundle/Controller/TipiController.php
@@ -1,23 +1,17 @@
 <?php
-
 namespace WCS\GestyBundle\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use WCS\GestyBundle\Form\Model\TipiModel;
 use WCS\GestyBundle\Form\Type\TipiType;
-
 class TipiController extends Controller
 {
-
-
     public function formAction(Request $request)
     {
         $tipiModel = new TipiModel();
         $form = $this->createForm(new TipiType(), $tipiModel);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-
             $alert = NULL;
             /* R&eacute;cup&eacute;ration des valeurs des champs du formulaire */
             if (get_magic_quotes_gpc())
@@ -34,11 +28,9 @@ class TipiController extends Controller
                 $mel	     	= trim($tipiModel->getMel());
                 $refdet	= trim($tipiModel->getRefdet());
             }
-
             /* Expression r&eacute;gulière permettant de v&eacute;rifier qu'aucun
             * en-tête n'est ins&eacute;r&eacute; dans nos champs */
             $regex_head = '/[\n\r]/';
-
             /* On v&eacute;rifie que tous les champs sont remplis */
             if (empty($montant)
                 || empty($mel)
@@ -65,8 +57,6 @@ class TipiController extends Controller
                 return $this->redirect('https://www.tipi.budget.gouv.fr/tpa/paiement.web?numcli=012327&exer=\'.$date.\'&refdet=\'.$date.$refdet.\'&montant=\'.$montant.\'&mel=\'.$mel.\'&urlcl=http:/www.gesty.fr&saisie=A');
             }
         }
-
-
         return $this->render('WCSGestyBundle::tipi.html.twig', array(
             'form' => $form->createView()
         ));
